941c776613d3d71c066cb909bc2d7c99e170f214
Narayanan V Eswar <narayananveswar@pop-os.localdomain>
2021-05-23 13:48:20 +0530

    print prime number sieve of eratostheses

---
+++ printprime.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+
+
+void findPrime(int n)
+{
+    int prime[100] = {0};
+    for(int i=2;i<n;i++)
+    {
+        if(prime[i] == 0)
+        {
+            for(int j=i*i;j<n;j+=i)
+            {
+                prime[j] = 1;
+            }
+        }
+    }
+    for(int i=2;i<n;i++)
+    {
+        if(prime[i] == 0)
+        {
+            cout<<(i)<<" ";        }
+    }
+    cout<<endl;
+}
+
+int main()
+{
+    int upperlimt;
+    cin>>upperlimt;
+    findPrime(upperlimt);
+}
